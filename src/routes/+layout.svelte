c8aee0a4cc654f8d85d13f43bfc68405f5b6f0d3
Dominik Czechowski <dczechowski@interia.pl>
2024-06-14 00:24:42 +0200

    responsive styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,28 +24,45 @@
 	}
 </script>
 
-<h1>GW2 Helper</h1>
-<fieldset>
-	<legend>API settings</legend>
-	<p>
-		In order to use this site you have to provide an API key for your account. API keys may be created or deleted at <a
-			href="https://account.arena.net/applications">https://account.arena.net/applications.</a
-		>.
-	</p>
-	<label for="api-key">Your API key:</label>
-	<input type="text" name="api-key" id="api-key" class="apikey" placeholder="Paste your API key here" bind:value={apiKey} />
-	<button on:click={() => saveApiKey()}>Apply</button>
-	<button on:click={() => deleteApiKey()}>Forget stored key</button>
-	<button on:click={refresh}>refresh</button>
-</fieldset>
-
-<nav id="main-nav">
-	<a href="{base}/">Home</a>
-	<a href="{base}/characters/" data-sveltekit-preload-data="tap">Characters</a>
-	<a href="{base}/items/" data-sveltekit-preload-data="tap">Items</a>
-	<a href="{base}/materials/" data-sveltekit-preload-data="tap">Materials</a>
-</nav>
+<header>
+	<h1>GW2 Helper</h1>
+	<fieldset>
+		<legend>API settings</legend>
+		<p>
+			In order to use this site you have to provide an API key for your account. API keys may be created or deleted at <a
+				href="https://account.arena.net/applications">https://account.arena.net/applications.</a
+			>.
+		</p>
+		<label for="api-key">Your API key:</label>
+		<input type="text" name="api-key" id="api-key" class="apikey" placeholder="Paste your API key here" bind:value={apiKey} />
+		<button on:click={() => saveApiKey()}>Apply</button>
+		<button on:click={() => deleteApiKey()}>Forget stored key</button>
+		<button on:click={refresh}>refresh</button>
+	</fieldset>
+	
+	<nav id="main-nav">
+		<a href="{base}/">Home</a>
+		<a href="{base}/characters/" data-sveltekit-preload-data="tap">Characters</a>
+		<a href="{base}/items/" data-sveltekit-preload-data="tap">Items</a>
+		<a href="{base}/materials/" data-sveltekit-preload-data="tap">Materials</a>
+	</nav>
+</header>
 
 <main>
 	<slot />
 </main>
+
+<style lang="scss" global>
+	header{
+		margin: 0 1rem;
+	}
+	nav {
+		margin: 0.5rem 1rem;
+		a {
+			padding: 0.3rem 1rem;
+		}
+	}
+	main{
+		margin: 0 1rem;
+	}
+</style>
--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -1,103 +1,135 @@
 <script>
-    import { base } from '$app/paths';
-    export let data;
+	import { base } from '$app/paths';
+	export let data;
 
-    data.characters.sort((a,b) => -1 * (a.age - b.age))
+	data.characters.sort((a, b) => -1 * (a.age - b.age));
 
-    function diff(createdAt){
-        const dt = new Date(createdAt)
-        return Math.floor(((new Date()).getTime() - dt.getTime()) / (1000 * 3600 * 24));
-    }
-    function tillBirthday(createdAt){
-        return 365 - (diff(createdAt) % 365);
-    }
+	function diff(createdAt) {
+		const dt = new Date(createdAt);
+		return Math.floor((new Date().getTime() - dt.getTime()) / (1000 * 3600 * 24));
+	}
+	function tillBirthday(createdAt) {
+		return 365 - (diff(createdAt) % 365);
+	}
 
-    function age(createdAt) {
-        return Math.floor(diff(createdAt) / 365)
-    }
+	function age(createdAt) {
+		return Math.floor(diff(createdAt) / 365);
+	}
 
-    function hoursPlayed(time) {
-        return Math.trunc(time / 3600);
-    }
+	function hoursPlayed(time) {
+		return Math.trunc(time / 3600);
+	}
 
-    function icon(name){
-        return `${base}/assets/${name}`;
-    }
+	function icon(name) {
+		return `${base}/assets/${name}`;
+	}
 
-    function iconOpacity(createdAt){
-        let perc = (1 - tillBirthday(createdAt)/365);
-        return perc < 0.1 ? 0.1 : perc;
-    }
+	function iconOpacity(createdAt) {
+		let perc = 1 - tillBirthday(createdAt) / 365;
+		return perc < 0.1 ? 0.1 : perc;
+	}
 
-    function iconScale(createdAt){
-        return 28 + (365 - tillBirthday(createdAt))/365 * 100;
-    }
+	function iconScale(createdAt) {
+		return 28 + ((365 - tillBirthday(createdAt)) / 365) * 100;
+	}
 
-    function iconPosition(createdAt){
-        return (128 - iconScale(createdAt)) / 2;
-    }
-
+	function iconPosition(createdAt) {
+		return (128 - iconScale(createdAt)) / 2;
+	}
 </script>
+
 <h1>Characters</h1>
 <!-- <pre>
     {JSON.stringify(data, true)}
 </pre> -->
 {#each data.characters as char}
-<article class="character">
-    <h2>{char.name}</h2>
-    <section style="background-image: url({icon(char.profession+'_icon.png')});'">
-        <h4>{char.profession}</h4>
-        <div class="info">hours played</div>
-        <div class="counter">{hoursPlayed(char.age)}</div>
-    </section>
-    <section style="background-image: url({icon('Present_quaggan_icon.png')}); background-size: {iconScale(char.created)}px; background-position: {iconPosition(char.created)}px center;">
-        <div class="counter">{age(char.created)} years</div>
-        <div class="info">next birthday in</div>
-        <div class="counter">{tillBirthday(char.created)}<span class="info">&nbsp;days</span></div>
-    </section>
-    <section style="background-image: url({icon('Grave_Finisher.png')});">
-        <div class="info">died</div>
-        <div class="counter">{char.deaths}</div>
-        <div class="info">times</div>
-    </section>
-</article>
+	<article class="character">
+		<h2>{char.name}</h2>
+		<section style="background-image: url({icon(char.profession + '_icon.png')});">
+			<h4>{char.profession}</h4>
+			<div class="info">hours played</div>
+			<div class="counter">{hoursPlayed(char.age)}</div>
+		</section>
+		<section
+			style="background-image: url({icon('Present_quaggan_icon.png')}); background-size: {iconScale(char.created)}px; background-position: {iconPosition(
+				char.created
+			)}px top;"
+		>
+			<div class="counter">{age(char.created)} years</div>
+			<div class="info">next birthday in</div>
+			<div class="counter">{tillBirthday(char.created)}<span class="info">&nbsp;days</span></div>
+		</section>
+		<section style="background-image: url({icon('Grave_Finisher.png')});">
+			<div class="info">died</div>
+			<div class="counter">{char.deaths}</div>
+			<div class="info">times</div>
+		</section>
+	</article>
 {/each}
 
 <style lang="scss">
-    article {
-        width: 900px;
-        margin: 10px 0;
-        padding: 10px;
-        display: flex;
-        flex-flow: row wrap;
-        column-gap: 20px;
-        background-color: #dcdcdc;
-        h2{
-            width: 100%;
-            margin: 0;
-        }
-        section{
+	article {
+		margin: 10px 0;
+		padding: 10px;
+		display: flex;
+		flex-flow: row wrap;
+        justify-content: space-around;
+		column-gap: 20px;
+		background-color: #dcdcdc;
+		h2 {
+			width: 100%;
+            margin: 0.2rem 0 1rem 0;
+			padding: 0;
+            text-align: center;
+		}
+		section {
+            min-width: 150px;
             overflow: hidden;
-            margin: 0;
-            padding: 0;
-            width: 280px;
-            height: 150px;
-            display: flex;
-            flex-flow: column nowrap;
-            background-repeat: no-repeat;
-            justify-content: center;
-            align-items: flex-start;
-            padding: 0 0 0 140px;
-            background-position: left center;            
-        }
-        .counter{
-            padding: 0.5rem 0;
-            font-size: x-large;
-            font-weight: bold;
-        }
-        .info{
-            font-size: 1rem;
-            font-weight: normal;
-        }
-    }
+			margin: 0;
+			padding: 0;
+			display: flex;
+			flex-flow: column nowrap;
+			background-repeat: no-repeat;
+			justify-content: center;
+			align-items: center;
+			padding: 120px 0 0 0;
+			background-position: top center;
+		}
+		.counter {
+			padding: 0.5rem 0;
+			font-size: x-large;
+			font-weight: bold;
+		}
+		.info {
+			font-size: 1rem;
+			font-weight: normal;
+		}
+	}
+
+	@media (min-width: 900px) {
+		article {
+            width: 900px;
+            h2{
+                text-align: left;
+            }
+			section {
+                width: 280px;
+                height: 150px;
+                padding: 0 0 0 140px;
+				background-position: left center;
+                flex-flow: column nowrap;
+                align-items: flex-start;
+			}
+			.counter {
+				padding: 0.5rem 0;
+				font-size: x-large;
+				font-weight: bold;
+			}
+			.info {
+				font-size: 1rem;
+				font-weight: normal;
+			}
+		}
+	}
+
 </style>
