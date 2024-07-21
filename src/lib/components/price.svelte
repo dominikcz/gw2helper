9a038bf75d0b1557857439cebafb91aec521dad9
Dominik Czechowski <dczechowski@interia.pl>
2024-07-21 23:05:12 +0200

    better prezentation for coins when value == 0

--- src/lib/components/price.svelte
+++ src/lib/components/price.svelte
@@ -1,14 +1,18 @@
 <script>
-    export let value;
-    export let compact = true;
-    const s = (value / 100).toFixed(2);
-    const [decimal, copper] = s.split('.'); 
-    const silver = decimal.slice(-2);
-    const gold = decimal.slice(0, -2);
+	export let value;
+	export let compact = true;
+	const s = (value / 100).toFixed(2);
+	const [decimal, copper] = s.split('.');
+	const silver = decimal.slice(-2);
+	const gold = decimal.slice(0, -2);
 </script>
 
 <span class="price">
-    {#if gold}<span class="gold">{gold} </span><i class="gold" title="gold coin"/>{/if}
-    {#if silver && (!compact || silver != '00')}<span class="silver">{silver} </span><i class="silver" title="silver coin"/>{/if}
-    {#if !compact || copper != '00'}<span class="copper">{copper} </span><i class="copper" title="copper coin"/>{/if}
+	{#if value == 0}
+		<span class="copper">0 </span><i class="copper" title="copper coin" />
+	{:else}
+		{#if gold}<span class="gold">{gold} </span><i class="gold" title="gold coin" />{/if}
+		{#if silver && (!compact || silver != '00')}<span class="silver">{silver} </span><i class="silver" title="silver coin" />{/if}
+		{#if !compact || copper != '00'}<span class="copper">{copper} </span><i class="copper" title="copper coin" />{/if}
+	{/if}
 </span>
