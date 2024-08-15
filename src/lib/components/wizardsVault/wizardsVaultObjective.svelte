8bd3269e0c7aa611797802b30d2940b0763a92e6
Dominik Czechowski <dczechowski@interia.pl>
2024-08-15 08:54:40 +0200

    WIP: Wizard's Vault

---
+++ src/lib/components/wizardsVault/wizardsVaultObjective.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    interface WizardsVaultObjective {
+      id: number,
+      title: string,
+      track: "WvW" | "PvE" | "PVP",
+      acclaim: number,
+      progress_current: number,
+      progress_complete: number,
+      claimed: boolean
+    }
+
+    export let value: WizardsVaultObjective;
+
+    function getClasses(){
+        let cssClass = `category-${value.track}`;
+        if (value.claimed) {
+            cssClass += "claimed";
+        }
+        return cssClass;
+    }
+
+</script>
+
+<div class="category {getClasses()}">
+    <span>{value.title}</span>
+    <progress value={value.progress_current <= value.progress_complete ? value.progress_current : value.progress_complete} max={value.progress_complete} /> 
+    <div>{value.acclaim}</div>
+</div>
+
+
+<style lang="scss">
+    .category{
+        .category-WvW{
+            background: #3b310d url(/gw2helper/assets/rewards/Wizards_Vault_WvW.png) no-repeat center center;
+        }
+    }
+</style>
--- src/routes/daily/+page.svelte
+++ src/routes/daily/+page.svelte
@@ -1,5 +1,34 @@
+<script>
+	import WizardsVaultObjective from "$lib/components/wizardsVault/wizardsVaultObjective.svelte";
+    import { weekly } from './weekly';
+</script>
 
 <img src="/gw2helper/assets/150px-construction.png" title="Under constrution" width="150px" alt="under construction"/>
 
+<h1>Wizard's Vault</h1>
 
-to be continued...
+
+<details>
+    <summary>Daily <progress value=1 max=4/></summary>
+    <article>
+
+    </article>
+</details>
+
+<details>
+    <summary>Weekly</summary>
+    <article>
+        {#each weekly.objectives as value}
+        <WizardsVaultObjective {value} />
+        {/each}
+    </article>
+</details>
+
+<details>
+    <summary>Special</summary>
+    <article>
+
+    </article>
+</details>
+
+<h1>Wizard's Vault</h1>
