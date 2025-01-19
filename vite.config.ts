ca7867b5d4ce2aacfd6e59d64815698026534b5b
dominikcz <dczechowski@interia.pl>
2025-01-19 10:29:53 +0100

    package update and compatibility with node v22

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
